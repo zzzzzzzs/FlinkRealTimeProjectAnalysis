41c49e03650ae4c117856ebd093ca4b080dd163b
zzzzzzzs <1443539042@qq.com>
2022-03-28 21:57:01 +0800

    update

--- gmall-realtime/src/main/java/com/me/gmall/realtime/common/KafkaConfig.java
+++ gmall-realtime/src/main/java/com/me/gmall/realtime/common/KafkaConfig.java
@@ -2,7 +2,7 @@ package com.me.gmall.realtime.common;
 
 public class KafkaConfig {
 //    public static final String KAFKA_SERVER = "hadoop102:9092,hadoop103:9092,hadoop104:9092";
-    public static final String KAFKA_SERVER = "kafkamachine:9092";
+    public static final String KAFKA_SERVER = "node1:9092";
     public static final String ODSTOPIC = "ods_base_log";
     public static final String ODSGROUPID = "base_log_app_group";
 
